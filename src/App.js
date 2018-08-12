a18393146a19b5b27d076e654690a69a0d298219
UMAR AZHAR SYED <18468181@students.latrobe.edu.au>
2018-08-12 23:48:10 +1000

    Some Changes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Smart Notes React App</h1>
         </header>
         <p className="App-intro">
+		  We are still in process of building the app.
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
